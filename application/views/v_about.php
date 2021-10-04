ac94a99a03f9a8b86023579ad2641831668af86f
anisadianangraini <anisadian70@gmail.com>
2021-10-04 17:35:03 +0700

    mengedit autoload dan menambahkan file template sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<selection>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama </h4>
+    <ul type="disc">
+        <li>Nama depan : Imam </li>
+        <li>Nama belakang : Nawawi </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Ciledug Raya No. 168 Peanggrahan</li>
+    </ul>
+
+    <h4>Tempat Lahir </h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olahraga Favorit </h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</selection>
