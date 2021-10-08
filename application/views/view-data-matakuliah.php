760d5f6e9ab0a59a40c5fb2a8d81e7a2a1dd08ea
anisadianangraini <anisadian70@gmail.com>
2021-10-08 15:28:24 +0700

    menambahkan form validation pada matakuliah.php

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -1,52 +1,51 @@
 <html>
 
-<head>
-    <tittle>Tampil Data Matakuliah</tittle>
-</head>
+    <head>
+        <tittle>Tampil Data Matakuliah</tittle>
+    </head>
 
-<body>
-    <center>
-            <table>
-                <tr>
-                    <th colspan="3">
-                        Tampil Data Matakuliah
-                    </th>
-                </tr>
-                <tr>
-                    <td colspan="3">
-                        <hr>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Kode MTK</th>
-                    <th>:</th>
-                    <td>
-                        <?=$kode; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Nama MTK</th>
-                    <td>:</td>
-                    <td>
-                        <?$nama; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th>SKS</th>
-                    <td>:</td>
-                    <td>
-                       <?= $sks; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="3" align="center">
-                        <a href="<?= base_url('matakuliah'); ?>">kembali</a>   
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </center>
-</body>
+    <body>
+        <center>
+                <table>
+                    <tr>
+                        <th colspan="3">
+                            Tampil Data Matakuliah
+                        </th>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <hr>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Kode MTK</th>
+                        <th>:</th>
+                        <td>
+                            <?=$kode; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Nama MTK</td>
+                        <td>:</td>
+                        <td>
+                            <?=$nama; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>SKS</td>
+                        <td>:</td>
+                        <td>
+                            <?= $sks; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" align="center">
+                            <a href="<?= base_url('matakuliah'); ?>">kembali</a>   
+                        </td>
+                    </tr>
+                </table>
+        </center>
+    </body>
 
 </html>
 
